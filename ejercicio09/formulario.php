b9236502be7e251131e5f0458192b225825ed72b
lore1 <lorena.araroj@educa.jcyl.es>
2022-11-10 12:25:04 +0000

    acabado validaciones

--- ejercicio09/formulario.php
+++ ejercicio09/formulario.php
@@ -1,5 +1,5 @@
 <?php
-    require('./validaciones.php');
+require('./validaciones.php');
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -17,32 +17,32 @@
         <p>
             <label for="nombre">Nombre</label>
             <input type="text" name="nombre" id="idNombre" placeholder="Nombre" value="<?php
-            if(enviado() && !vacio('nombre') && nombre('nombre')){
+            if (enviado() && !vacio('nombre') && nombre('nombre')) {
                 echo $_REQUEST['nombre'];
             }
             ?>">
             <?php
-                if(vacio('nombre') && enviado()){
-                    
+            if (vacio('nombre') && enviado()) {
+
             ?>
-             <span style="color:red">El nombre no puede estar vacio</span>
+            <span style="color:red">El nombre no puede estar vacio</span>
             <?php
-                     } 
-            ?>
-            <?php
-            if(enviado() && !vacio('nombre') && !nombre('nombre')){
-                ?>
-                <span style="color:red">EL nombre debe de ser valido(minimo 3 letras)</span>
-                <?php
             }
             ?>
+            <?php
+            if (enviado() && !vacio('nombre') && !nombre('nombre')) {
+            ?>
+            <span style="color:red">EL nombre debe de ser valido(minimo 3 letras)</span>
+            <?php
+            }
+                ?>
         </p>
         <p>
             <label for="apellido">Apellido:</label>
             <input type="text" name="apellido" id="idApellido" placeholder="Apellidos" value="<?php
-                if(enviado() && !vacio('apellido') && apellido('apellido')){
-                    echo $_REQUEST['apellido'];
-                }
+            if (enviado() && !vacio('apellido') && apellido('apellido')) {
+                echo $_REQUEST['apellido'];
+            }
             ?>">
             <?
                 if(vacio('apellido') && enviado()){
@@ -52,7 +52,8 @@
                 }
                 if(enviado() && !vacio('apellido') && !apellido('apellido')){
             ?>
-            <span style="color:red">El apellido debe de ser valido (un minimo de 3 carecteres espacio 3 caracteres)</span>
+            <span style="color:red">El apellido debe de ser valido (un minimo de 3 carecteres espacio 3
+                caracteres)</span>
             <?
                 }
             ?>
@@ -60,8 +61,8 @@
         </p>
         <p>
             <label for="fecha">Fecha Nacimiento:</label>
-            <input type="text" name="fecha" id="idFecha" placeholder="Fecha" value="<?php
-            if(enviado() && !vacio('fecha') && !fecha('fecha')){
+            <input type="text" name="fecha" id="idFecha" placeholder="yyyy/mm/dd" value="<?php
+            if (enviado() && !vacio('fecha') && fecha('fecha')) {
                 echo $_REQUEST['fecha'];
             }
             ?>">
@@ -75,16 +76,59 @@
             ?>
             <span style="color:red">La fecha tiene que tener un formato valido (09/05/2004)</span>
             <?
-            }
+            } 
+            elseif(enviado() && !vacio('fecha') && !mayorEdad('fecha')){
+                ?>
+            <span style="color:red">Tienes que ser mayor de edad</span>
+            <?
+                }
             ?>
         </p>
         <p>
             <label for="dni">DNI</label>
-            <input type="text" name="dni" id="idDni" placeholder="DNI">
+            <input type="text" name="dni" id="idDni" placeholder="DNI" value="<?
+                if(enviado() &&!vacio('dni') && dni('dni')){
+                    echo $_REQUEST['dni'];
+                }
+            ?>">
+            <?
+                if(vacio('dni') && enviado()){
+            ?>
+
+            <span style="color:red">EL DNI no puede estar vacio</span>
+            <?
+            }
+            if(enviado() && !vacio('dni') && !dni('dni')){
+            ?>
+            <span style="color:red">El dni tiene que tener un formato valido </span>
+            <?
+            }elseif(enviado() && !vacio('dni') && !letraDni('dni')){
+            ?>
+            <span style="color:red">La letra debe de coincidir con los numeros</span>
+            <?
+            }
+        ?>
+
+
         </p>
         <p>
             <label for="correo">Correo:</label>
-            <input type="text" name="correo" id="idCorreo" placeholder="correo">
+            <input type="text" name="correo" id="idCorreo" placeholder="correo" value="<?
+                if(enviado() && !vacio('correo') && correo('correo')){
+                    echo $_REQUEST['correo'];
+                }
+            ?>">
+            <?
+                if(enviado()&& vacio('correo')){
+            ?>
+            <span style="color:red">El correo no puede estar vacio</span>
+            <?        
+                }if(enviado() && !vacio('correo') && !correo('correo')){
+                    ?>
+            <span style="color:red">El correo tiene que tener un formato valido </span>
+            <?
+                    }
+            ?>
         </p>
         <input type="submit" value="Enviar" name="enviar">
     </form>
--- ejercicio09/validaciones.php
+++ ejercicio09/validaciones.php
@@ -26,18 +26,45 @@ function apellido($apellido){
     return false;
 }
 function fecha($fecha){
-    $patron= '/[0-9]{2}\/[0-9]{2}\/[0,9]{4}/';
+    $patron='/[0-9]{4}\/[0-9]{2}\/[0-9]{2}/';
     if(preg_match($patron, $_REQUEST[$fecha])==1){
-        return true;
         //calcular edad
-        $fechaHoy= date("Y-m-d");
-        $edad= date_diff(date_create($fecha), date_create($fechaHoy));
-        if($edad>=18){
-            echo "Eres mayor de edad";
-        }else{
-            echo "Eres menor de edad";
-        }
+      return true;
         
+        
+    }
+    return false;
+}
+function mayorEdad($fecha){
+    $fechaHoy= date("Y-m-d");
+    $edad= date_diff(date_create($_REQUEST['fecha']), date_create($fechaHoy));
+    if($edad->y>=18){
+        return true;
+    }else{
+       return false;
+    }
+}
+function dni($dni){
+    $patron = '/[0-9]{8}[a-zA-Z]{1}/';
+    if(preg_match($patron, $_REQUEST[$dni])==1){
+        return true;
+    }
+    return false;
+}
+function letraDni($dni){
+    $letra=strtoupper(substr($_REQUEST['dni'], -1));
+    $numero= (int) substr($_REQUEST['dni'], 0, -1);
+    $palabro = 'TRWAGMYFPDXBNJZSQVHLCKE';  
+    if(substr($palabro, $numero%23, 1)==$letra){
+        return true;
+    }else{
+        return false;
+    }
+}
+function correo($correo){
+    $patron='/^[a-zA-Z0-9\.]+@+[a-zA-Z]+(\.)+[a-zA-Z]{2,3}/';
+    if(preg_match($patron, $_REQUEST[$correo])==1){
+        return true;
     }
     return false;
 }
