9281be0f14fd19fcef1f3c4f609c5fd9d1d57feb
lore1 <lorena.araroj@educa.jcyl.es>
2022-11-14 09:15:09 +0000

    fichero

---
+++ ejercicio10/eligeFichero.php
@@ -0,0 +1,42 @@
+<?
+//si ha sido enviado el formulario comprobar si ha sido leer o editar
+
+    if(isset($_REQUEST['leer']) || isset($_REQUEST['editar'])){
+        if(isset($_REQUEST['leer'])){
+            if(!file_exists($_REQUEST['nombre'])){
+                echo 'Debe introducir un nombre de fichero valido';
+            }else{
+                header('Location: ./leer.php?nombre='. $_REQUEST['nombre']);
+                exit;
+            }
+        }else{
+            header('Location: ./editar.php?nombre='. $_REQUEST['nombre']);
+                exit;
+        }
+    }
+    
+
+//si es leer comprobar que exista y si no existe se carga el formulario
+//si existe me envia con un header el nombre del fichero como parametro
+//si es ediatr un header si existe le pasa el nombre si no lo crea 
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fichero</title>
+</head>
+<body>
+    <form action="./eligeFichero.php" method="post" enctype="multipart/form-data">
+        <p>
+    <label for="nombre">Nombre del fichero:</label>
+    <input type="text" name="nombre" id="nombre">
+    </p>
+    <input type="submit" value="Editar" name="editar">
+    <input type="submit" value="Leer" name="leer">
+    </form>
+</body>
+</html>
+
---
+++ ejercicio10/leer.php
@@ -0,0 +1,32 @@
+<?
+if(isset($_REQUEST['volver'])){
+    header('Location: ./eligeFichero.php');
+    exit;
+}
+if(isset($_REQUEST['editar'])){
+    header('Location: ./editar.php?nombre='.$_REQUEST['nombre']);
+    //input hidden
+    exit;
+}
+//en caso de que haya pulsado leer nos dirige a esta pagina 
+   
+       if($archivo=fopen($_REQUEST['nombre'], 'r')){
+        $leido = fread($archivo, filesize($_REQUEST['nombre']));
+
+    ?>
+     <form action="./leer.php" method="post" enctype="multipart/form-data">
+        <input type="hidden" name="nombre" value="<? echo $_REQUEST['nombre']; ?>">
+    <textarea name="fichero" id="fichero" cols="30" rows="10" readonly><?
+            echo $leido;
+            fclose($archivo);
+        ?></textarea>
+    <?
+        
+    }
+?>
+        <input type="submit" name="volver" value="Volver">
+        <input type="submit" name="editar" value="Editar">
+     </form>
+<?
+
+?>
