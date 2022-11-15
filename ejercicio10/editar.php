2eb0055e8b50c2bbc1d9cd1823833a3326d5cd52
lorenaara <80786373+lorenaara@users.noreply.github.com>
2022-11-15 12:33:49 +0100

    Update editar.php

---
+++ ejercicio10/editar.php
@@ -0,0 +1,22 @@
+<?
+if(isset($_REQUEST['volver'])){
+    header('Location: ./eligeFichero.php');
+    exit;
+}
+if($fp=fopen($_REQUEST['nombre'], 'w')){
+    $leido = fread($fp, filesize($_REQUEST['nombre']));
+    ?>
+    <form action="./editar.php" method="post" enctype="multipart/form-data">
+    <input type="hidden" name="nombre" value="<? echo $_REQUEST['nombre']; ?>"> 
+    
+<textarea name="fichero" id="fichero" cols="30" rows="10"><?
+        echo $leido;
+        fwrite($fp, $leido, strlen($leido));
+        fclose($fp);
+    ?></textarea>
+  <input type="submit" name="volver" value="Volver">
+  <?   
+}
+
+
+?>
