8b4d813d96a4f84b56a26b6d1674515e41c377ce
lorenaara <80786373+lorenaara@users.noreply.github.com>
2022-11-23 18:27:51 +0100

    Create editar.php

---
+++ ejercicio11/editar.php
@@ -0,0 +1,26 @@
+<?php
+$dom= new DOMDocument();
+$dom->load('notasAlumnos.xml');
+
+echo '<table style="border: #000 1px solid; border-collapse: collapse; ">';
+echo '<tr><th style="border: #000 1px solid; padding:10px;">Alumno</th>';
+echo '<th style="border: #000 1px solid; padding:10px;">nota1</th>';
+echo '<th style="border: #000 1px solid; padding:10px;">nota2</th>';
+echo '<th style="border: #000 1px solid; padding:10px;">nota3</th></tr>';
+
+$raiz=$dom->childNodes[0]; //notas
+
+echo '<tr>';
+foreach($raiz->childNodes as $alumnos){
+    if($alumnos->nodeType==1){
+        foreach($alumnos->childNodes as $notas){
+            if($notas->nodeType ==1 && $_REQUEST['name']== $notas->nodeValue){
+                echo '<td style="border: #000 1px solid; text-align:center;">  '. $notas->nodeValue . '</td>';
+            }
+        }
+    }
+}
+echo ' <form action="./editar.php" method="post" enctype="multipart/form-data">';
+
+
+?>
