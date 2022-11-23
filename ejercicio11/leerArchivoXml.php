2f694b8fdedb67eee4797962a0a86ec62ff5196d
lore1 <lorena.araroj@educa.jcyl.es>
2022-11-23 09:13:45 +0000

    ejercicio11

---
+++ ejercicio11/leerArchivoXml.php
@@ -0,0 +1,26 @@
+<?
+$dom= new DOMDocument();
+//cargar fichero
+$dom->load('notasAlumnos.xml');
+echo '<table style="border: #000 1px solid; border-collapse: collapse; ">';
+echo '<tr><th style="border: #000 1px solid; padding:10px;">Alumno</th>';
+echo '<th style="border: #000 1px solid; padding:10px;">nota1</th>';
+echo '<th style="border: #000 1px solid; padding:10px;">nota2</th>';
+echo '<th style="border: #000 1px solid; padding:10px;">nota3</th></tr>';
+
+$raiz=$dom->childNodes[0]; //notas
+echo '<tr>';
+foreach($raiz->childNodes as $alumnos){
+    if($alumnos->nodeType==1){
+        foreach($alumnos->childNodes as $notas){
+            if($notas->nodeType ==1){
+                echo '<td style="border: #000 1px solid; text-align:center;">  '. $notas->nodeValue . '</td>';
+            }
+        }
+        echo '<td style="border: #000 1px solid; padding:10px;"><a href="editar.php">Editar</a></td>';
+        echo '</tr>';  
+    }
+}
+echo '</table>';
+
+?>
--- ejercicio11/transformaFichero.php
+++ ejercicio11/transformaFichero.php
@@ -1,5 +1,32 @@
 <?
 //Realizar una pagina que transforme el csv notas a un xml
+$dom = new DOMDocument('1.0','utf-8' );
+$dom->formatOutput= true;
+$raiz =$dom->createElement('notas');
+$dom->appendChild($raiz);
+if($fp= fopen('notas.csv', 'r')){
+    while($leido= fgetcsv($fp, filesize('notas.csv'), ';')){
+        //for($i=0; $i < count($leido); $i++){
+           // print_r($leido);
+            $alumno= $raiz->appendChild($dom->createElement('alumno'));
+            $alumno->appendChild($dom->createElement('nombre', $leido[0]));
+            $alumno->appendChild($dom->createElement('nota1', $leido[1]));
+            $alumno->appendChild($dom->createElement('nota2', $leido[2]));
+            $alumno->appendChild($dom->createElement('nota3', $leido[3]));
+
+        //}
+    }
+    if($dom->save('notasAlumnos.xml')){
+        echo 'Todo correcto';
+    }else{
+        echo 'Error';
+    }
+    fclose($fp);
+    //mandar a la pagina leer
+    header('Location:./leerArchivoXml.php');
+    exit;
+
+}
 
 
 ?>
