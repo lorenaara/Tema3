ee8d098d3389892c928c3b49e544983503f3e044
lore1 <lorena.araroj@educa.jcyl.es>
2023-01-09 08:19:49 +0000

    afq

---
+++ Tema5/ejemploCookies/funciones/funcionesBD.php
@@ -0,0 +1,12 @@
+<?
+    require '../seguro/conexion.php';
+//findAll Todos los productos
+function findAll(){
+    $con = new PDO('mysql:host'. HOST . ';dbname=' . BBDD, USER, PASS);
+}
+
+//findById Que devuelva uno para el ver
+function findById($id){
+
+}
+
